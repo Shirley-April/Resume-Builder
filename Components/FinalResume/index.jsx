142cbb97cc6c7d94da4d510ce4634d29254e5025
Shirley-April <shirleyapril23@gmail.com>
2023-06-01 16:47:42 +0300

    final resume preview

--- Components/FinalResume/index.jsx
+++ Components/FinalResume/index.jsx
@@ -1,19 +1,32 @@
-import { Grid, Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography, Button } from "@mui/material";
 import Resume from "../../Atoms/Resume";
 
 import Preview from "../../Atoms/Preview";
 
 const FinalResume = ({ resume }) => {
+  const sections = ["Contact", "Summary", "Experience", "Education", "Skills"];
   console.log("Props", resume);
   return (
-    <Stack alignItems="center" justifyContent="center" border={5} px={6}>
-      <Typography>Final Preview</Typography>
-      <Grid container  justifyContent="center">
-        <Grid item md={6}>
-            <Preview props={resume} />
+    <Stack alignItems="center" justifyContent="center" px={20}>
+      <Typography>Resume Name</Typography>
+      <Grid container justifyContent="center" spacing={3}>
+        <Grid item md={9}>
+          <Preview props={resume} />
         </Grid>
-        <Grid item md={5}>
-            <Typography>Actions here</Typography>
+        <Grid item md={3} >
+          <Stack height="100%" >
+            <Stack>
+              <Typography sx={{ fontWeight: "bold" }}>
+                RESUME SECTIONS
+              </Typography>
+              {sections.map((section, index) => (
+                <Typography key={index}>{section}</Typography>
+              ))}
+            </Stack>
+            <Stack>
+              <Button variant="contained">Download</Button>
+            </Stack>
+          </Stack>
         </Grid>
       </Grid>
     </Stack>
