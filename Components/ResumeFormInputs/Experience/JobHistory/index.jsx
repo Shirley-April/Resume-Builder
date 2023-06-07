f8fb45c38c1ff1cf8cfd3ae9586e64a6add22258
Shirley-April <shirleyapril23@gmail.com>
2023-06-07 19:11:26 +0300

    edit job experience

--- Components/ResumeFormInputs/Experience/JobHistory/index.jsx
+++ Components/ResumeFormInputs/Experience/JobHistory/index.jsx
@@ -1,26 +1,61 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Typography, Stack, Box, Button } from "@mui/material";
-import WorkExperience from "../../WorkExperience";
 
-const JobHistory = ({ form, handleOpenForm, handleCloseForm, setInputs, inputs }) => {
+import {
+  Typography,
+  Stack,
+  Box,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import WorkExperience from "../../WorkExperience";
+import EditExperence from "../EditExperience";
+
+const JobHistory = ({
+  form,
+  handleOpenForm,
+  handleCloseForm,
+  setInputs,
+  inputs,
+}) => {
   const jobHistory = useSelector((state) => state.resume.workExperience);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   console.log("History", jobHistory);
+
   return (
-    <Stack>
+    <Stack spacing={5}>
       <Stack spacing={1}>
         {Object.entries(jobHistory).map(([key, value], index) => (
           <Box key={index}>
-            {console.log({key, "Val": value.description?.split("\n")})}
-            <Typography variant="h6">
-              {value.jobTitle} | {value.company}
-            </Typography>
-
-            {value.description?.split("\n").map((duty, index) => (
-              <Typography sx={{ fontSize: 12 }} key={index}>
-                • {duty}
-              </Typography>
-            ))}
+            <Accordion
+              expanded={expanded === value.id}
+              onChange={handleChange(value.id)}
+              elevation={0}
+              sx={{ border: 1 }}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>
+                  {value.jobTitle} | {value.company}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>Display Form Here!</Typography>
+                <EditExperence
+                  setInputs={setInputs}
+                  inputs={inputs}
+                  value={value}
+                />
+              </AccordionDetails>
+            </Accordion>
           </Box>
         ))}
         {form ? null : (
@@ -28,21 +63,10 @@ const JobHistory = ({ form, handleOpenForm, handleCloseForm, setInputs, inputs }
             Add Experience
           </Button>
         )}
-        {/* {props.workExperience.map((experience, index) => (
-          <Box key={index}>
-            <Typography variant="h3">
-              {experience.jobTitle} | {experience.company}
-            </Typography>
-
-            {experience.description.split("\n").map((duty, index) => (
-              <Typography key={index}>• {duty}</Typography>
-            ))}
-          </Box>
-        ))} */}
       </Stack>
       {form && (
         <Stack spacing={2}>
-          <WorkExperience setInputs={setInputs} inputs={inputs}/>{" "}
+          <WorkExperience setInputs={setInputs} inputs={inputs} />{" "}
           <Button variant="contained" onClick={handleCloseForm}>
             Close
           </Button>
