f8cf9883d270f86d7f254a804fea8595b70c5732
Shirley-April <shirleyapril23@gmail.com>
2023-08-21 13:50:34 +0300

    work exp date input fields

--- Components/ResumeFormInputs/WorkExperience.jsx
+++ Components/ResumeFormInputs/WorkExperience.jsx
@@ -4,6 +4,10 @@ import { useFormikContext } from "formik";
 
 import { Stack, Grid, Box, Button } from "@mui/material";
 
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 import FormikCustomInput from "../../Atoms/FormikCustomInput";
 
 import { addExperience } from "../../features/resumeSlice";
@@ -34,14 +38,49 @@ const WorkExperience = ({ handleCloseForm, inputs, values, newExperience }) => {
   return (
     <Stack rowGap={3}>
       <Grid container>
-        <Stack width="100%" direction="row" columnGap={3}>
-          {workExperience.map((experience) => (
-            <Grid item key={experience} md={6} xs={12}>
-              <Stack spacing={2}>
-                <FormikCustomInput name={`newExperience.${experience}`} />
+        <Stack spacing={3} width="100%">
+          <Stack width="100%" direction="row" columnGap={3}>
+            {workExperience.map((experience) => (
+              <Grid item key={experience} md={12} xs={12}>
+                <Stack spacing={2}>
+                  <FormikCustomInput name={`newExperience.${experience}`} />
+                </Stack>
+              </Grid>
+            ))}
+          </Stack>
+          <Grid item md={12}>
+            <Stack direction="row" width="100%" columnGap={3}>
+              <Stack width="100%">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    name="newExperience.startDate"
+                    label={"Start Date"}
+                    views={["month", "year"]}
+                    onChange={(newValue) =>
+                      formik.setFieldValue(
+                        "newExperience.startDate",
+                        newValue.format("MMMM YYYY")
+                      )
+                    }
+                  />
+                </LocalizationProvider>
               </Stack>
-            </Grid>
-          ))}
+              <Stack width="100%">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label={"End Date"}
+                    views={["month", "year"]}
+                    onChange={(newValue) =>
+                      formik.setFieldValue(
+                        "newExperience.endDate",
+                        newValue.format("MMMM YYYY")
+                      )
+                    }
+                  />
+                </LocalizationProvider>
+              </Stack>
+            </Stack>
+          </Grid>
         </Stack>
       </Grid>
       <Box>
