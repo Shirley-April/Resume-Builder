135f1e2e7b2844b335cdfb5a4f42d75b8b1062cd
Shirley-April <shirleyapril23@gmail.com>
2023-08-22 19:19:18 +0300

    redux handle creation of more than one resume

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
 import { useRouter } from "next/router";
 
+import { useDispatch } from "react-redux";
+
 import { Stack, Typography, Box } from "@mui/material";
+import { addResume } from "../features/resumeSlice";
 
 const Home = () => {
   const router = useRouter();
+  const dispatch = useDispatch()
+
+  const newResume = {
+    resumeName: "",
+    contact: {
+      jobTitle: "",
+      name: "Shirley",
+      phone: "",
+      email: "",
+      linkedin: "",
+      github: "",
+    },
+    summary: "",
+    workExperience: [],
+    skills: "",
+    education: [],
+  }
+
+  const handleCreateResume = () => {
+    dispatch(addResume(newResume))
+    router.push("/create-resume")
+  }
+
   return (
     <Stack justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
       <Stack
         alignItems="center"
         sx={{ border: 1, borderRadius: 2, py: 8, px: 4, cursor: "pointer" }}
-        onClick={() => router.push("/create-resume")}
+        onClick={handleCreateResume}
       >
         <Typography>+</Typography>
         <Typography>Create resume</Typography>
