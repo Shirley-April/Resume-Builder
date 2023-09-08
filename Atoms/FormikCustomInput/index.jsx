e579f5871ef48d51ac34e38728bef033141fab99
Shirley-April <shirleyapril23@gmail.com>
2023-09-08 10:13:34 +0300

    custom input code cleaning

--- Atoms/FormikCustomInput/index.jsx
+++ Atoms/FormikCustomInput/index.jsx
@@ -1,32 +1,12 @@
-import { useState, useEffect } from "react";
+import { Field, ErrorMessage, getIn } from "formik";
 
-import { Field, ErrorMessage, getIn, FastField } from "formik";
-import PropTypes from "prop-types";
-
-import {
-  TextField,
-  IconButton,
-  InputAdornment,
-  Tooltip,
-  MenuItem,
-  Typography,
-  InputLabel,
-} from "@mui/material";
-
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-
-// import { withStyles } from "@mui/styles";
+import { TextField, MenuItem } from "@mui/material";
 
 const FormikCustomInput = ({
   type,
-  pClass,
   label,
   variant,
   name,
-  haveTooltip,
-  tooltipText,
   multiline,
   required,
   error,
@@ -35,18 +15,8 @@ const FormikCustomInput = ({
   selectItem,
   onChange,
   onBlur,
-  fastField,
-  shrink,
-  disabled,
-  autoFocus,
   ...restProps
 }) => {
-  const [visibility, setVisibility] = useState(false);
-
-
-  const typeChangeHandler = () => {
-    setVisibility((prev) => !prev);
-  };
 
   return (
     <Field name={name}>
@@ -54,12 +24,9 @@ const FormikCustomInput = ({
         return (
           <TextField
             {...field}
-            label={<Typography variant="subtitle3">{label}</Typography>}
             id={label}
             variant={variant}
-            type={
-              type !== "password" ? type : visibility ? "text" : "password"
-            }
+            type="text"
             name={name}
             onChange={onChange !== null ? onChange : form.handleChange}
             onBlur={onBlur !== null ? onBlur : form.handleBlur}
@@ -68,7 +35,7 @@ const FormikCustomInput = ({
             error={Boolean(
               getIn(form.touched, name) && getIn(form.errors, name)
             )}
-            InputProps={{ style: { listStyleType: 'disc' } }} // Add bullet points style to textarea
+            InputProps={{ style: { listStyleType: "disc" } }} // Add bullet points style to textarea
             placeholder={label}
             size="medium"
             fullWidth
@@ -87,49 +54,6 @@ const FormikCustomInput = ({
       }}
     </Field>
   );
-};
-
-FormikCustomInput.defaultProps = {
-  pClass: "",
-  tooltipText: "",
-  label: "",
-  haveTooltip: false,
-  multiline: false,
-  variant: "outlined",
-  required: false,
-  error: false,
-  helperText: "",
-  select: false,
-  selectItem: [],
-  onChange: null,
-  onBlur: null,
-  fastField: true,
-  shrink: false,
-  type: "text",
-  disabled: false,
-  autoFocus: false,
-};
-
-FormikCustomInput.propTypes = {
-  type: PropTypes.string,
-  label: PropTypes.string,
-  onChange: PropTypes.func,
-  onBlur: PropTypes.func,
-  name: PropTypes.string.isRequired,
-  haveTooltip: PropTypes.bool,
-  tooltipText: PropTypes.string,
-  multiline: PropTypes.bool,
-  pClass: PropTypes.string,
-  variant: PropTypes.string,
-  required: PropTypes.bool,
-  error: PropTypes.bool,
-  helperText: PropTypes.string,
-  select: PropTypes.bool,
-  selectItem: PropTypes.arrayOf(PropTypes.shape({})),
-  fastField: PropTypes.bool,
-  shrink: PropTypes.bool,
-  disabled: PropTypes.bool,
-  autoFocus: PropTypes.bool,
 };
 
 export default FormikCustomInput;
