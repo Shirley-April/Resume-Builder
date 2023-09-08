8c45f559b819bbf9e1dd890db0a024b69a9a5111
Shirley-April <shirleyapril23@gmail.com>
2023-09-08 09:14:38 +0300

    style contact section

--- Components/ResumeFormInputs/Contact.jsx
+++ Components/ResumeFormInputs/Contact.jsx
@@ -3,25 +3,28 @@ import FormikCustomInput from "../../Atoms/FormikCustomInput";
 
 const Contact = () => {
   const contactInfo = [
-    "jobTitle",
-    "name",
-    "phone",
-    "email",
-    "linkedin",
-    "github",
+    { name: "jobTitle", label: "Job Title" },
+    { name: "name", label: "Name" },
+    { name: "phone", label: "Phone" },
+    { name: "email", label: "Email" },
+    { name: "github", label: "Github" },
+    { name: "linkedin", label: "LinkedIn" },
   ];
-
-  const labels = ["Job Title", "Name", "Phone", "Email", "LinkedIn", "GitHub"];
 
   return (
     <Box>
-      <Typography variant="h5">Contact Info</Typography>
-      <Grid container spacing={3}>
+      <Typography variant="h5" fontWeight="bold">
+        Contact Info
+      </Typography>
+      <Grid container spacing={3} pt={0.5}>
         {contactInfo.map((contact, index) => (
           <Grid item xs={12} md={6} key={index}>
-            <Stack spacing={2}>
-              <Typography>{contact}</Typography>
-              <FormikCustomInput name={`contact.${contact}`} id={contact} />
+            <Stack spacing={0.5}>
+              <Typography>{contact.label}</Typography>
+              <FormikCustomInput
+                name={`contact.${contact.name}`}
+                id={`contact.${contact.name}`}
+              />
             </Stack>
           </Grid>
         ))}
