766be105d91d09b8647a032fa1389bfa9ac464bb
Shirley-April <shirleyapril23@gmail.com>
2023-09-11 14:57:30 +0300

    live preview theme

--- Components/ResumePreview/index.jsx
+++ Components/ResumePreview/index.jsx
@@ -1,7 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, createTheme } from "@mui/material";
+
 import Resume from "../../Atoms/Resume";
 
-const ResumePreview = ({resume}) => {
+const LivePreview = ({ resume }) => {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: 20,
+        fontWeight: "bold",
+      },
+      h2: {
+        fontSize: 12,
+        fontWeight: "bold",
+      },
+      h3: {
+        fontSize: 10,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        color: "#181818",
+      },
+      body1: {
+        fontSize: 10,
+      },
+      subtitlebold: {
+        fontWeight: "bold",
+        fontSize: 10,
+      },
+    },
+  });
   return (
     <Box
       px={4}
@@ -15,9 +41,9 @@ const ResumePreview = ({resume}) => {
         height: "100vh",
       }}
     >
-      <Resume resume={resume}/>
+      <Resume resume={resume} theme={theme} />
     </Box>
   );
 };
 
-export default ResumePreview;
+export default LivePreview;
