ef5feab88019355efbcd4359de44d494cb39b1ac
Shirley-April <shirleyapril23@gmail.com>
2023-09-11 16:10:28 +0300

    final resume theme

--- Components/FinalResume/index.jsx
+++ Components/FinalResume/index.jsx
@@ -1,20 +1,44 @@
-import { Grid, Stack, Typography, Button } from "@mui/material";
+import { Grid, Stack, Typography, Button, createTheme } from "@mui/material";
 import Resume from "../../Atoms/Resume";
 
-import Preview from "../../Atoms/Preview";
-
 const FinalResume = ({ resume }) => {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: 28,
+        fontWeight: "bold",
+      },
+      h2: {
+        fontSize: 16,
+        fontWeight: "bold",
+      },
+      h3: {
+        fontSize: 14,
+        fontWeight: "bold",
+      },
+      body1: {
+        fontSize: 14,
+      },
+      subtitlebold: {
+        fontWeight: "bold",
+        fontSize: 10,
+      },
+      listFont: {
+        fontSize: 14
+      }
+    },
+  });
   const sections = ["Contact", "Summary", "Experience", "Education", "Skills"];
-  console.log("Props", resume);
+
   return (
     <Stack alignItems="center" justifyContent="center" px={20}>
       <Typography>Resume Name</Typography>
       <Grid container justifyContent="center" spacing={3}>
         <Grid item md={9}>
-          <Preview props={resume} />
+          <Resume resume={resume} theme={theme}/>
         </Grid>
-        <Grid item md={3} >
-          <Stack height="100%" >
+        <Grid item md={3}>
+          <Stack height="100%">
             <Stack>
               <Typography sx={{ fontWeight: "bold" }}>
                 RESUME SECTIONS
