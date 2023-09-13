7fc06db324b63cf67126700c842ade90244d98e4
Shirley-April <shirleyapril23@gmail.com>
2023-09-13 16:15:51 +0300

    restore props validation

--- Atoms/FormikCustomInput/index.jsx
+++ Atoms/FormikCustomInput/index.jsx
@@ -1,4 +1,5 @@
 import { Field, ErrorMessage, getIn } from "formik";
+import PropTypes from "prop-types";
 
 import { TextField, MenuItem } from "@mui/material";
 
@@ -15,6 +16,9 @@ const FormikCustomInput = ({
   selectItem,
   onChange,
   onBlur,
+  shrink,
+  disabled,
+  autoFocus,
   ...restProps
 }) => {
   return (
@@ -25,7 +29,7 @@ const FormikCustomInput = ({
             {...field}
             id={label}
             variant={variant}
-            type={"text"}
+            type="text"
             name={name}
             onChange={onChange !== null ? onChange : form.handleChange}
             onBlur={onBlur !== null ? onBlur : form.handleBlur}
@@ -52,6 +56,39 @@ const FormikCustomInput = ({
       }}
     </Field>
   );
+};
+
+FormikCustomInput.defaultProps = {
+  label: "",
+  multiline: false,
+  variant: "outlined",
+  required: false,
+  error: false,
+  helperText: "",
+  select: false,
+  selectItem: [],
+  onChange: null,
+  onBlur: null,
+  shrink: false,
+  type: "text",
+  disabled: false,
+};
+
+FormikCustomInput.propTypes = {
+  type: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  multiline: PropTypes.bool,
+  variant: PropTypes.string,
+  required: PropTypes.bool,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
+  select: PropTypes.bool,
+  selectItem: PropTypes.arrayOf(PropTypes.shape({})),
+  shrink: PropTypes.bool,
+  disabled: PropTypes.bool,
 };
 
 export default FormikCustomInput;
