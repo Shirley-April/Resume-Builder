5ef71def78488a52d45372cdf541b5e53fce804b
Shirley-April <shirleyapril23@gmail.com>
2023-09-19 19:03:59 +0300

    code refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,13 @@
 import { useRouter } from "next/router";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { Stack, Typography, Box } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { addResume } from "../features/resumeSlice";
-import { useEffect } from "react";
 
 const Home = () => {
   const router = useRouter();
   const dispatch = useDispatch();
-
-  const resume = useSelector((state) => state.resume);
-
-  let resumeId;
-
-  resume.length > 0 ? (resumeId = resume.at(-1).id) : null;
 
   const newResume = {
     resumeName: "",
@@ -34,26 +27,17 @@ const Home = () => {
 
   const handleCreateResume = () => {
     dispatch(addResume(newResume));
+    router.push({
+      pathname: "/create-resume",
+    });
   };
-
-  useEffect(() => {
-    // Wait for the resume to update in the Redux store
-    if (resume.length > 0) {
-      const resumeId = resume.slice(-1)[0].id;
-
-      router.push({
-        pathname: "/create-resume",
-        query: { id: resumeId },
-      });
-    }
-  }, [resume]);
 
   return (
     <Stack justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
       <Stack
         alignItems="center"
         sx={{ border: 1, borderRadius: 2, py: 8, px: 4, cursor: "pointer" }}
-        onClick={handleCreateResume}
+        onClick={handleCreateResume()}
       >
         <Typography>+</Typography>
         <Typography>Create resume</Typography>
